0b8e09e35debd45a1924cf1bc671c0c4a3093200
NiharSanoria1 <140265080+NiharSanoria1@users.noreply.github.com>
2023-09-02 21:26:35 +0530

    Add files via upload

---
+++ InputOutput2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class InputOutput2{
+    public static void main(String[] args){
+        Scanner sc  =  new Scanner(System.in);
+
+        int i = sc.nextInt();
+        double d = sc.nextDouble();        
+        Scanner in = new Scanner(System.in);
+        String  s = in.nextLine();
+         System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
